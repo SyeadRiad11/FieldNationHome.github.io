39ee09bfa7a58eae2e34bd7db55e8ce8480a74e7
Syead Riad <77849887+SyeadRiad11@users.noreply.github.com>
2021-03-29 20:01:23 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+
+
+
+function loadFunction(){
+    alert("This website is not responsive.You will get a horrible view if you are using mobile to visit this website. Only dekstop view recommended. Thanks - Syead Riad.");
+}
